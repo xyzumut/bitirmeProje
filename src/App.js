c96d7a337da776bb86f90befeb0fd78580020312
xyzumut <91182554+xyzumut@users.noreply.github.com>
2022-11-06 21:14:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,32 +4,20 @@ import KanbanPanosu from './pages/KanbanPanosu/KanbanPanosu';
 import Header from './components/header/Header';
 import AnaSayfa from './pages/AnaSayfa/Anasayfa';
 import Login from './pages/Login/Login';
-import { ThemeContextProvider, useTheme } from './context/ThemeContext';
-import styled from 'styled-components';
+import { ThemeContextProvider } from './context/ThemeContext';
 
 const App = () => {
 
   return (
     <>
-<<<<<<< HEAD
       <ThemeContextProvider>
         <Header></Header>
         <Routes>
-          <Route  path='/' element={<AnaSayfa/>}></Route>
+          <Route path='/' element={<AnaSayfa/>}></Route>
           <Route path='/kanban' element={<KanbanPanosu/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
         </Routes>
       </ThemeContextProvider>
-=======
-
-      <Header></Header> 
-      
-      <Routes>
-        <Route  path='/' element={<AnaSayfa/>}></Route>
-        <Route path='/kanban' element={<KanbanPanosu/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-      </Routes>
->>>>>>> 53dd0928b8313bd12e5e1dc1c14e75d80ac6c705
     </>
   );
 }
